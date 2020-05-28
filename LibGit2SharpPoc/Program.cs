a82020a149c456aac726ef8b501e7905b4f473bc
litetex <litetex.dev@gmail.com>
2020-05-28 21:14:59 +0200

    Added Poc

---
+++ LibGit2SharpPoc/Program.cs
@@ -0,0 +1,13 @@
+﻿using LibGit2Sharp;
+using System;
+
+namespace LibGit2SharpPoc
+{
+   static class Program
+   {
+      static void Main(string[] args)
+      {
+         Repository.Discover("");
+      }
+   }
+}
